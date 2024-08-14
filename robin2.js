bc54a473b7fa013699b9a9f2b79c0ab647832845
charles young <charlesyoung@charless-MacBook-Pro-2.local>
2024-08-13 22:30:54 -0400

    Initial commit

---
+++ robin2.js
@@ -0,0 +1,19 @@
+// Part 1: Humble Beginnings
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+      name: "Leo",
+      type: "Cat",
+      companion: {
+        name: "Frank",
+        type: "Flea",
+        inventory: ["small hat", "sunglasses"]
+      }
+    },
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+      }
+    };
